b713b5e2eeefda5ef6bc92dc7e52b0b3d8888834
GermanPaul12 <german.paul@icloud.com>
2025-01-17 08:06:56 +0100

    lsp config  update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,11 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "clangd", "cmake", "cssls", "html"}
+                ensure_installed = {"lua_ls", "clangd", 
+					"cmake", "cssls", "html", "bashls",
+				"ast_grep", "jinja_lsp", "dockerls",
+				"gopls", "hyprls", "jsonls", "ltex", 
+					"ruff", "rust_analyzer", "sqls", "yamlls"}
             })
         end
     },
@@ -18,8 +22,15 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
+	    lspconfig.ruff.setup({})
+	    lspconfig.rust_analyzer.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+	    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+	    vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
+
+-- :h vim.lsp.buf
+-- get help for lsp what commands or options are available
 
